ba6fa05de3cfefde194fc7e8ef0aa7250c620bae
Franklin Ikeh <ikehfranklin06@gmail.com>
2025-10-03 16:49:45 +0100

    feat(client): initialize client project with package.json and tsconfig.json

---
+++ client/app/index.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
